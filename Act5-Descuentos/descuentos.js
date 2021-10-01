54200d72a23fe8a7cf55896c663d083117f34463
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2021-10-01 14:31:40 -0300

    resolucion desafio clase 2

--- Act5-Descuentos/descuentos.js
+++
@@ -1,8 +0,0 @@
-let cafeM = prompt ("Precio del café con medialunas")
-
-cafeM = Number (cafeM)
-
-let d30 = 0.3;
-let d20 = 0.2;
-
-console.log ("Precio con 30% de descuento: " + (cafeM - (cafeM*d30)) + "\n" + "Precio con 20% de descuento: " + (cafeM - (cafeM*d20)) );
