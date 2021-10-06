ca9368ce1d8b968021776a06a781c3d56a4818e3
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2021-10-06 15:32:16 -0300

    creacion archivos

---
+++ ciclos.js
@@ -0,0 +1,13 @@
+// >> Consigna: Tomando como base los ejemplos anteriores de la estructura for y while, crear un algoritmo que repita un bloque de instrucciones. En cada repetición es necesario efectuar una operación o comparación para obtener una salida por alerta o consola.
+// >>Aspectos a incluir en el entregable:
+// Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta <script src="js/miarchivo.js"></script>, que incluya la definición de un algoritmo en JavaScript que emplee bucles e instrucciones condicionales.
+
+
+// Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
+// Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”.
+// Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
+
+
+for (i=0; i<10; i++){
+    
+}
