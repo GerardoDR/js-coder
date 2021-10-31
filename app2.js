d616b878ca86b1af463a21aa36d2803e1653b54b
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2021-10-30 21:16:30 -0300

    nuevo nav + hero; solo html y css

--- app2.js
+++ app2.js
@@ -1,4 +1,10 @@
-//CLASS Y CREACIÓN DE OBJETOS
+/////////////////////////////////VARIABLES
+
+let producto;
+let peso;
+let cantidad;
+
+/////////////////////////////////CLASS Y CREACIÓN DE OBJETOS
 class Producto {
 
     constructor(id, nombre, peso, precio, marca, material, stock) {
@@ -21,22 +27,63 @@ class Producto {
     restarStock = (cantDemandada) => {
         cantDemandada = parseInt(cantDemandada)
         if (this.stock >= cantDemandada) {
-            this.stock -= cantDemandada
+            this.stock -= cantDemandada;
         } else {
-            alert("stock insuficiente")
+            alert("stock insuficiente");
         }
     }
 }
 
-productos.push(new Producto(0, "barra", 15, 16000, "Rogue", "acero", 30))
-productos.push(new Producto(1, "barra", 20, 19000, "Rogue", "acero", 30))
-productos.push(new Producto(2, "disco", 5, 2800, "Rogue", "caucho", 60))
-productos.push(new Producto(3, "disco", 10, 5800, "Rogue", "caucho", 60))
-productos.push(new Producto(4, "mancuerna", 5, 1600, "Barbell", "acero", 30))
-productos.push(new Producto(5, "mancuerna", 7, 2200, "Barbell", "acero", 30))
-productos.push(new Producto(6, "mancuerna", 10, 3000, "Barbell", "acero", 30))
+///////////////////////////FUNCIONES
 
-let producto
-let peso
-let cantidad
+const desc = (cantidad) => {
+    if(cantidad >= 5 && cantidad < 10){
+        console.log('Se aplica 5% de descuento');
+        return 5;
+    }else if(cantidad >= 10){
+        console.log('Se aplica 10% de descuento');
+        return 10;
+    }else{
+        console.log("descuento mayorista no aplicable");
+        return 0;
+    }
+}
 
+const stockPrecio = (producto, peso, cantidad) => {
+    let precio;
+    let stock;
+    if (!isNaN(cantidad)) {
+        productos.forEach(element => {
+            if (element.nombre === producto && element.peso === peso) {
+                stock = element.verificarStock(cantidad);
+                if (stock === true) {
+                    precio = element.precio;
+                    console.log("Stock disponible!");
+                } else if (stock === false) {
+                    precio = false;
+                    console.log("Stock insuficiente. Solo tenemos " + element.stock + " unidades");
+                    alert("Stock insuficiente. Tenemos " + element.stock + " unidades disponibles.");
+                }
+            }
+        })
+    } else {
+        alert("Ingreso de cantidad erroneo. Por favor, ingrese los detalles nuevamente.");
+        console.log("Valor de cantidad: " + cantidad + ". Ingreso erroneo");
+        precio = false;
+    }
+    return precio;
+}
+
+//funcion que se introduce en calculo()
+const tot = (precio, descuento) => (precio - (precio * (descuento / 100)));
+
+//devuelve precio final
+const calculo = (precio, descuento, impuesto, cantidad) => {
+    let total = tot(precio, descuento);
+    console.log("precio unitario bruto con descuento = $" + total);
+    total = total * impuesto;
+    console.log("precio unitario con descuento e impuesto = $" + total);
+    total = total * cantidad;
+    console.log("precio final= $" + total.toFixed(2) + " ==> " + cantidad + " unidad/es.");
+    return total;
+}
