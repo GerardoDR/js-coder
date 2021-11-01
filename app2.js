ad1c3d7c45947e2759e25a85fd2f725d6c728f09
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2021-10-31 23:53:32 -0300

    unificación de páginas + intento de reparar error de visualización al cambiar clase de contenedor

--- app2.js
+++ app2.js
@@ -3,18 +3,20 @@
 let producto;
 let peso;
 let cantidad;
+const productos = [];
 
 /////////////////////////////////CLASS Y CREACIÓN DE OBJETOS
 class Producto {
 
-    constructor(id, nombre, peso, precio, marca, material, stock) {
+    constructor(id, nombre, peso, precio, marca, material, stock,img) {
         this.id = id,
-        this.nombre = nombre,
-        this.peso = peso,
-        this.precio = precio,
-        this.marca = marca,
-        this.material = material,
-        this.stock = stock
+            this.nombre = nombre,
+            this.peso = peso,
+            this.precio = precio,
+            this.marca = marca,
+            this.material = material,
+            this.stock = stock,
+            this.img = img
     }
     verificarStock = (cantDemandada) => {
         if (this.stock >= cantDemandada) {
@@ -25,7 +27,7 @@ class Producto {
     }
 
     restarStock = (cantDemandada) => {
-        cantDemandada = parseInt(cantDemandada)
+        cantDemandada = parseInt(cantDemandada);
         if (this.stock >= cantDemandada) {
             this.stock -= cantDemandada;
         } else {
@@ -34,16 +36,24 @@ class Producto {
     }
 }
 
+productos.push(new Producto(0, "barra", 15, 16000, "Quuz", "acero", 30,'barra.jpg'));
+productos.push(new Producto(1, "barra", 20, 19000, "Quuz", "acero", 30,'barra.jpg'));
+productos.push(new Producto(2, "disco", 5, 2800, "Quuz", "caucho", 60,'disco.jpg'));
+productos.push(new Producto(3, "disco", 10, 5800, "Quuz", "caucho", 60,'disco.jpg'));
+productos.push(new Producto(4, "mancuerna", 5, 1600, "Quuz", "acero", 30,'mancuerna.jpg'));
+productos.push(new Producto(5, "mancuerna", 7, 2200, "Quuz", "acero", 30, 'mancuerna.jpg'));
+productos.push(new Producto(6, "mancuerna", 10, 3000, "Quuz", "acero", 30, 'mancuerna.jpg'));
+
 ///////////////////////////FUNCIONES
 
 const desc = (cantidad) => {
-    if(cantidad >= 5 && cantidad < 10){
+    if (cantidad >= 5 && cantidad < 10) {
         console.log('Se aplica 5% de descuento');
         return 5;
-    }else if(cantidad >= 10){
+    } else if (cantidad >= 10) {
         console.log('Se aplica 10% de descuento');
         return 10;
-    }else{
+    } else {
         console.log("descuento mayorista no aplicable");
         return 0;
     }
@@ -52,25 +62,19 @@ const desc = (cantidad) => {
 const stockPrecio = (producto, peso, cantidad) => {
     let precio;
     let stock;
-    if (!isNaN(cantidad)) {
-        productos.forEach(element => {
-            if (element.nombre === producto && element.peso === peso) {
-                stock = element.verificarStock(cantidad);
-                if (stock === true) {
-                    precio = element.precio;
-                    console.log("Stock disponible!");
-                } else if (stock === false) {
-                    precio = false;
-                    console.log("Stock insuficiente. Solo tenemos " + element.stock + " unidades");
-                    alert("Stock insuficiente. Tenemos " + element.stock + " unidades disponibles.");
-                }
+    productos.forEach(element => {
+        if (element.nombre === producto && element.peso === peso) {
+            stock = element.verificarStock(cantidad);
+            if (stock === true) {
+                precio = element.precio;
+                console.log("Stock disponible!");
+            } else if (stock === false) {
+                precio = false;
+                console.log("Stock insuficiente. Solo tenemos " + element.stock + " unidades");
+                alert("Stock insuficiente. Tenemos " + element.stock + " unidades disponibles.");
             }
-        })
-    } else {
-        alert("Ingreso de cantidad erroneo. Por favor, ingrese los detalles nuevamente.");
-        console.log("Valor de cantidad: " + cantidad + ". Ingreso erroneo");
-        precio = false;
-    }
+        }
+    })
     return precio;
 }
 
@@ -87,3 +91,62 @@ const calculo = (precio, descuento, impuesto, cantidad) => {
     console.log("precio final= $" + total.toFixed(2) + " ==> " + cantidad + " unidad/es.");
     return total;
 }
+
+/////////////////////////////////
+
+const qProd = document.querySelectorAll('.qProd');
+const prodMain = document.querySelector('.prodMain');
+const cont = document.querySelector('.contenedor');
+const home = document.querySelector('.home');
+cont.id = 'contenedor-index1';
+console.log(cont)
+
+const filtro = (arr,nombre) => {
+    const filtrado = arr.filter((prod) => prod.nombre === nombre);
+    return filtrado;
+}
+
+const listarProductos = (arrayFiltrado) => {
+    prodMain.innerHTML="";
+    arrayFiltrado.forEach((e) => {
+        const creaDiv = document.createElement('div');
+        creaDiv.classList.add('prodMain__elemento');
+        creaDiv.innerHTML= `
+        <h3 class="caps">${e.nombre} ${e.peso} kg</h3>
+        <img src="/assets/${e.img}" alt="${e.nombre}">
+        <ul>
+            <li>Precio unitario: $${e.precio}</li>
+            <li>Marca: ${e.marca}</li>
+            <li>Material: ${e.material}</li>
+        </ul>
+        `;
+        prodMain.append(creaDiv);
+    })
+}
+
+home.addEventListener('click', () => cont.id = "contenedor-index1")
+
+qProd.addEventListener('click', () => cont.id='contenedor-index2')
+
+qProd.forEach((e) => e.addEventListener('click', (event) => {
+    event.preventDefault()
+    if (e.className === 'qProd barra'){
+        const filtrado = filtro(productos,'barra');
+        console.log(filtrado)
+        const div = listarProductos(filtrado);
+    }else if(e.className === 'qProd disco'){
+        const filtrado = filtro(productos,'disco');
+        const div = listarProductos(filtrado)
+    }else if(e.className === 'qProd mancuerna'){
+        const filtrado = filtro(productos,'mancuerna');
+        const div = listarProductos(filtrado);
+    }else{
+        prodMain.innerHTML="";
+        const div = document.createElement('div');
+        div.classList.add('prodMain__elemento');
+        div.id = 'proximamente'
+        div.innerHTML= "<h2>PROXIMAMENTE!</h2>";
+        prodMain.append(div)
+    };
+
+}));
--- productos.js
+++
@@ -1,9 +0,0 @@
-const productos= [
-productos.push(new Producto(0, "barra", 15, 16000, "Rogue", "acero", 30)),
-productos.push(new Producto(1, "barra", 20, 19000, "Rogue", "acero", 30)),
-productos.push(new Producto(2, "disco", 5, 2800, "Rogue", "caucho", 60)),
-productos.push(new Producto(3, "disco", 10, 5800, "Rogue", "caucho", 60)),
-productos.push(new Producto(4, "mancuerna", 5, 1600, "Barbell", "acero", 30)),
-productos.push(new Producto(5, "mancuerna", 7, 2200, "Barbell", "acero", 30)),
-productos.push(new Producto(6, "mancuerna", 10, 3000, "Barbell", "acero", 30))
-]
