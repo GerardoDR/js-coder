9b6f203b83a9a78d87a7154f7bb9e3ee4dd97b44
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2021-11-02 19:51:18 -0300

    ajustes de estilo

--- app.js
+++ app.js
@@ -69,7 +69,6 @@ const listarProductos = (arrayFiltrado) => {
         creaDiv.classList.add('prodMain__elemento', 'col', 'mb-5');
         creaDiv.innerHTML =
             `<div class="card h-100">
-                <!-- <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div> -->
                 <img class="card-img-top" src="/assets/${e.img}" alt="${e.nombre}" />
                 <div class="card-body p-4">
                     <div class="text-center">
@@ -81,7 +80,6 @@ const listarProductos = (arrayFiltrado) => {
                     <div class="text-center"><a id="prod${e.id}" class="btn btn-outline-dark mt-auto">Agregar al carrito</a></div>
                 </div>
             </div>
-        </div>
         `;
 
         prodMain.append(creaDiv);
@@ -119,7 +117,7 @@ qProd.forEach((e) => e.addEventListener('click', (event) => {
         const div = document.createElement('div');
         div.classList.add('prodMain__elemento', 'col', 'mb-2');
         div.id = 'proximamente';
-        div.innerHTML = "<h5>Proximamente</h5>";
+        div.innerHTML = "<span>Proximamente</span>";
         prodMain.append(div);
     };
 }));
@@ -130,19 +128,16 @@ const actualizarCarrito = () => {
         const li = document.createElement('li')
         li.className = "d-flex flex-row align-items-center justify-content-between"
         li.innerHTML = `
-    <span class="caps nomProd">${prod.nombre} ${prod.peso} kg</span>
+    <span class="caps nomProd">${prod.nombre} ${prod.peso}kg.</span>
     <span>Precio: $${prod.precio}</span>
     <button id="carr${i}" class="btn text-danger"><i class="bi bi-x-circle"></i></button>
     `
         contCarrito.append(li)
-
         const botonEliminar = document.querySelector(`#carr${i}`);
-
         botonEliminar.addEventListener('click', () => {
             eliminarDelCarrito(i)
         });
     })
-    console.log(carrito)
     cantidadEnCarrito.innerText = carrito.length
     const pTotal = carrito.reduce((acc, carr) => acc + carr.precio, 0)
     total.innerText = `$${pTotal}`
@@ -158,12 +153,12 @@ const eliminarDelCarrito = (i) => {
     carrito.splice(i, 1)
     actualizarCarrito()
     if (carrito.length == 0) {
-        contCarrito.innerHTML = `<h5>Carrito vacío</h5>`
+        contCarrito.innerHTML = `<span class="caps nomProd">Carrito vacío</span>`
     }
 }
 
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
-    contCarrito.innerHTML = `<h5>Carrito vacío</h5>`
+    contCarrito.innerHTML = `<span class="caps nomProd">Carrito vacío</span>`
 })
